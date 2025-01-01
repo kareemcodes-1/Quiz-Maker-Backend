bb8449eddc2ffb397bd2a6bc45b617df9f51c3c8
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-01 10:33:54 +0100

    test

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.listen(port, () => {
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cors({
-    origin: [client],
+    origin: "*",
     methods: ["POST", "GET", "PUT", "DELETE"],
 }));
 
