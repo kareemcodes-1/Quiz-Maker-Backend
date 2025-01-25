c44059d8ae606bca0e845e4df969626e6f494aa1
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-25 16:18:46 +0100

    bruh

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -33,7 +33,7 @@ const getAllProjects = expressAsyncHandler(async (req, res) => {
         }
     } catch (error) {
         console.log(error);
-        res.status(500).json({message: "Server Error"});
+        res.status(500).json({message: error.message});
     }
 });
 
