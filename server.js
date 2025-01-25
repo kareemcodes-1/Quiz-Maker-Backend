c6680e64bcee7ccee0227284df08078f8e0fecf3
Kareem Braimoh <kareembrai222@gmail.com>
2025-01-25 16:16:35 +0100

    bruh

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(
     cors({
-      origin: "*", // Add client URL explicitly
+      origin: [client, "https://productivity-app-client-ebon.vercel.app"], // Add client URL explicitly
       credentials: true, // Allow cookies/auth headers
     })
   );
